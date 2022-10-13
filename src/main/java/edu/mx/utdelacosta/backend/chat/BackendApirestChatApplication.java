997c82e203b8d0cb1dc966829afc7c311e99f44a
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-10-13 10:15:58 -0600

    Configurando el socket

---
+++ src/main/java/edu/mx/utdelacosta/backend/chat/BackendApirestChatApplication.java
@@ -0,0 +1,13 @@
+package edu.mx.utdelacosta.backend.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApirestChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApirestChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mx/utdelacosta/backend/chat/BackendApirestChatApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mx.utdelacosta.backend.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApirestChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
