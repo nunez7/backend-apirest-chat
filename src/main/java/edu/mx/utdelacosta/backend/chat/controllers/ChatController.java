173c00dbaad0648b29dc1597472dd89657e33df0
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2022-10-13 11:21:24 -0600

    Creando el controlador y el mensaje

---
+++ src/main/java/edu/mx/utdelacosta/backend/chat/controllers/ChatController.java
@@ -0,0 +1,24 @@
+package edu.mx.utdelacosta.backend.chat.controllers;
+
+import java.util.Date;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import edu.mx.utdelacosta.backend.chat.models.documents.Mensaje;
+
+@Controller
+public class ChatController {
+	
+	@MessageMapping("/mensaje")
+	@SendTo("/chat/mensaje")
+	public Mensaje recibeMensaje(Mensaje mensaje) {
+		//Recibimos el mensaje
+		mensaje.setFecha(new Date().getTime());
+		mensaje.setTexto("Recibido por el broker: "+mensaje.getTexto());
+		
+		return mensaje;
+	}
+
+}
